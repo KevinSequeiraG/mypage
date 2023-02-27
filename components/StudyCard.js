e913b23f1659ad01faa3642aa4c1cb0dc5b5696c
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-02-26 20:48:38 -0600

    ASFD

--- components/StudyCard.js
+++ components/StudyCard.js
@@ -8,7 +8,7 @@ const StudyCard = (props) => {
     
 
     return(
-        <div data-aos={(props.isMobile?props.mobileAnimation:props.animation)} className="w-[85%] mx-auto border-2 border-[#B8B8B8] rounded-xl my-8 py-5 px-3 text-center">
+        <div data-aos={props.animation} className="w-[85%] mx-auto border-2 border-[#B8B8B8] rounded-xl my-8 py-5 px-3 text-center">
             <h1>{props.isMobile?'CEL':'PC'}</h1>
             <img className="rounded-xl max-w-[10rem] mx-auto" src={props.image}></img>
             <h2 className="my-2">{props.name}</h2>
