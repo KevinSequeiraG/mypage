9000568b154b78278a991665fc5e56fbd21ffb68
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-02-26 20:19:23 -0600

    a

--- components/StudyCard.js
+++ components/StudyCard.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 const StudyCard = (props) => {
-    const [isMobile, setIsMobile] = useState(navigator.userAgentData.mobile)
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+      setIsMobile(navigator.userAgentData.mobile)
+    }, [])
+    
 
     return(
         <div data-aos={isMobile?props.mobileAnimation:props.animation} className="w-[85%] mx-auto border-2 border-[#B8B8B8] rounded-xl my-8 py-5 px-3 text-center">
