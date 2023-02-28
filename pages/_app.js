0311a587023d75dc0a2a15687e6f4eeb6c445ddf
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-02-27 20:20:51 -0600

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import 'material-icons/iconfont/material-icons.css';
 import 'aos/dist/aos.css'
+import 'animate.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
