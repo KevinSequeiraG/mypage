1f93dd1866d27b409fd804f488855729cacaaf9e
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-12-14 18:13:21 -0600

    update

--- pages/Studies/index.js
+++ pages/Studies/index.js
@@ -38,7 +38,7 @@ const Studies = () => {
                     <h1 className="text-[1.8rem] font-bold text-center italic">{t["my-studies"]}</h1>
                 </div>
                 <div className="mdx1200:flex">
-                    <StudyCard animation={'zoom-in'} image={'../Images/utn.png'} name={"Universidad Técnica Nacional"} startMonth={"jan"} startYear={"2020"} finishMonth={"current"} finishYear={""} careerName={'software-engineering'} />
+                    <StudyCard animation={'zoom-in'} image={'../Images/utn.png'} name={"Universidad Técnica Nacional"} startMonth={"jan"} startYear={"2020"} finishMonth={"nov"} finishYear={"2023"} careerName={'software-engineering'} />
                     <StudyCard animation={'zoom-in'} image={'../Images/calufa.jpg'} name={"Colegio técnico nacional Carlos Luis Fallas"} startMonth={"feb"} startYear={"2017"} finishMonth={"dec"} finishYear={"2019"} careerName={'business-computing'} />
                     {/* <StudyCard animation={'zoom-in'} image={'../Images/grego.jpg'} name={"Colegio Grogorio José Ramírez"} startMonth={"feb"} startYear={"2012"} finishMonth={"dec"} finishYear={"2016"} careerName={'bachelor'} /> */}
                 </div>
